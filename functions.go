7ad9a4a7843aab49d018840ba8c5f0de2ec5d64e
MikeRedfox <mikiguarda@gmail.com>
2024-02-14 12:59:59 +0100

    added Newton method

--- functions.go
+++ functions.go
@@ -2,13 +2,14 @@ package numerigo
 
 import (
 	"errors"
+	"math"
 )
 
 func PuntoFisso[T int | int8 | int16 | int32 | int64 | float32 | float64](x0, toll T, f func(T) T, nmax int) (T, error) {
 
 	var diff T = 1.0
 	n := 1
-	var xs [100]T
+	xs := make([]T, int(nmax))
 	xs[0] = x0
 
 	for diff > toll && n < nmax {
@@ -56,5 +57,30 @@ func Simpson[T int | int8 | int16 | int32 | int64 | float32 | float64](f func(fl
 	}
 	I = float64(h) * float64(I) / float64(3)
 	return I
+
+}
+
+func Newton[T int | int8 | int16 | int32 | int64 | float32 | float64](f, df func(float64) float64, x0, toll T, nmax int) (float64, error) {
+	n := 0
+	flag := 0
+	diff := float64(toll) + 1
+	xs := make([]float64, int(nmax))
+	xs[0] = float64(x0)
+	for diff >= float64(toll) && n < nmax && flag == 0 {
+		if df(float64(xs[n])) == 0 {
+			flag = 1
+		} else {
+			diff = -f(float64(xs[n])) / df(float64(xs[n]))
+			xs[n+1] = float64(xs[n]) + diff
+			diff = math.Abs(diff)
+			n++
+		}
+	}
+
+	if flag == 1 {
+		return 0, errors.New("df(x) == 0. Can not use this algorithm")
+	}
+
+	return xs[n-1], nil
 
 }
